4e4456a69938c9060fa4d855cec5fa6de044f104
Gabriel Saliev <gumata.gumata@gmail.com>
2021-02-27 09:05:08 +0200

    javadoc defined

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -7,23 +7,23 @@ import org.bson.Document;
 
 public class App
 {
+    /**
+     * Connects to mongoDB, creates document, stores and retrieves document in the database.
+     * @param args
+     */
     public static void main(String[] args)
     {
-        // Connect to MongoDB
         MongoClient mongoClient = new MongoClient("mongo-dbserver");
-        // Get a database - will create when we use it
         MongoDatabase database = mongoClient.getDatabase("mydb");
-        // Get a collection from the database
         MongoCollection<Document> collection = database.getCollection("test");
-        // Create a document to store
+
         Document doc = new Document("name", "Kevin Sim")
                 .append("class", "Software Engineering Methods")
                 .append("year", "2021")
                 .append("result", new Document("CW", 95).append("EX", 85));
-        // Add document to collection
+
         collection.insertOne(doc);
 
-        // Check document in collection
         Document myDoc = collection.find().first();
         System.out.println(myDoc.toJson());
     }
