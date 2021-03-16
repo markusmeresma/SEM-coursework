4dbcd17b4c72dfce3d614fa90d13ab63c15bc4ab
Gabriel Saliev <gumata.gumata@gmail.com>
2021-03-16 16:41:06 +0200

    docker compose and pom update

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,30 +1,10 @@
 package com.napier.sem;
 
-import com.mongodb.MongoClient;
-import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.MongoCollection;
-import org.bson.Document;
+import java.sql.SQLOutput;
 
-public class App
-{
-    /**
-     * Connects to mongoDB, creates document, stores and retrieves document in the database.
-     * @param args
-     */
-    public static void main(String[] args)
-    {
-        MongoClient mongoClient = new MongoClient("mongo-dbserver");
-        MongoDatabase database = mongoClient.getDatabase("mydb");
-        MongoCollection<Document> collection = database.getCollection("test");
+public class App {
 
-        Document doc = new Document("name", "Kevin Sim")
-                .append("class", "Software Engineering Methods")
-                .append("year", "2021")
-                .append("result", new Document("CW", 95).append("EX", 85));
-
-        collection.insertOne(doc);
-
-        Document myDoc = collection.find().first();
-        System.out.println(myDoc.toJson());
+    public static void main(String[] args) {
+        System.out.println("No mongo");
     }
 }
