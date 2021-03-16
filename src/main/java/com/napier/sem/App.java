2553cbe176a7d85d296f29d86b4e9ae154b9eeb2
Gabriel Saliev <gumata.gumata@gmail.com>
2021-03-16 17:14:25 +0200

    db driver added

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,10 +1,18 @@
 package com.napier.sem;
 
+import com.napier.sem.driver.DBDriverMysql;
+
 import java.sql.SQLOutput;
 
 public class App {
+    private static final String DB_URL = "jdbc:mysql://db:3306/world?useSSL=false";
+    private static final String DB_PASSWORD = "semcoursework";
 
     public static void main(String[] args) {
-        System.out.println("No mongo");
+        DBDriverMysql db = new DBDriverMysql(DB_URL, DB_PASSWORD);
+
+        db.connect();
+        db.disconnect();
     }
+
 }
