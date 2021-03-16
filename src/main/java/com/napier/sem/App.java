e7939b62e92e55b6d844a8357a758594f0839d9d
Gabriel Saliev <gumata.gumata@gmail.com>
2021-03-16 18:05:25 +0200

    infrastructure ready

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,8 +1,11 @@
 package com.napier.sem;
 
 import com.napier.sem.driver.DBDriverMysql;
+import com.napier.sem.objects.Country;
+import com.napier.sem.queries.ContinentQueries;
 
 import java.sql.SQLOutput;
+import java.util.List;
 
 public class App {
     private static final String DB_URL = "jdbc:mysql://db:3306/world?useSSL=false";
@@ -12,6 +15,14 @@ public class App {
         DBDriverMysql db = new DBDriverMysql(DB_URL, DB_PASSWORD);
 
         db.connect();
+
+        ContinentQueries continentQueries = new ContinentQueries(db.getConn());
+
+        List<Country> countries = continentQueries.getPopulationDescending();
+
+        for (var country : countries) {
+            System.out.println(country.toString());
+        }
         db.disconnect();
     }
 
