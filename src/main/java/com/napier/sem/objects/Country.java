cabac126556b6f2bfc4035fbc006b837b4d10a56
Gabriel Saliev <gumata.gumata@gmail.com>
2021-03-16 18:04:28 +0200

    country model

---
+++ src/main/java/com/napier/sem/objects/Country.java
@@ -0,0 +1,29 @@
+package com.napier.sem.objects;
+
+public class Country {
+
+    private final String name;
+
+    private final String continent;
+
+    private final String region;
+
+    private final int population;
+
+    public Country(String name, String continent, String region, int population) {
+        this.name = name;
+        this.continent = continent;
+        this.region = region;
+        this.population = population;
+    }
+
+    @Override
+    public String toString() {
+        return "Country{" +
+                "name='" + name + '\'' +
+                ", continent='" + continent + '\'' +
+                ", region='" + region + '\'' +
+                ", population=" + population +
+                '}';
+    }
+}
