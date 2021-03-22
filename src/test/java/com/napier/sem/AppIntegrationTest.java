fc6abaa5328dd7fbd5c3e8043b2ebfb00acb9e07
Gabriel Saliev <gumata.gumata@gmail.com>
2021-03-22 21:43:38 +0200

    tests setup

---
+++ src/test/java/com/napier/sem/AppIntegrationTest.java
@@ -0,0 +1,17 @@
+package com.napier.sem;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class AppIntegrationTest {
+
+    @BeforeClass
+    public void setup() {
+
+    }
+
+    @Test
+    public void dummy() {
+
+    }
+}
---
+++ src/test/java/com/napier/sem/WorldQueriesTest.java
@@ -0,0 +1,16 @@
+package com.napier.sem;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * We need to use some mocking framework in order to test the queries
+ * I propose Mockito - Gabriel
+ */
+public class WorldQueriesTest {
+
+    @Test
+    public void emptyTest() {
+
+    }
+}
