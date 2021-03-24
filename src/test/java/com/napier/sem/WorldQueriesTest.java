5114941d34b5e89d23f984050c87a2a54a395bcd
Rory Gatens <40496042@live.napier.ac.uk>
2021-03-24 02:15:52 +0000

    Added return name function to Country.java Added Countries sorted by population in a given continent query to WorldQueriesTest.java

--- src/test/java/com/napier/sem/WorldQueriesTest.java
+++ src/test/java/com/napier/sem/WorldQueriesTest.java
@@ -1,7 +1,14 @@
 package com.napier.sem;
 
+import com.napier.sem.objects.Country;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * We need to use some mocking framework in order to test the queries
@@ -9,8 +16,26 @@ import org.junit.Test;
  */
 public class WorldQueriesTest {
 
-    @Test
-    public void emptyTest() {
+    private static App app;
 
+
+    @BeforeClass
+    public static void setup() {
+        app = new App();
+        app.connect("localhost:33060");
+    }
+
+    @Test
+    public void getContinents() {
+        List<Country> query = app.getContinentCountriesDescending("Africa");
+        String actualPopulation = query.get(0).getName();
+        String expectedPopulation = "Nigeria";
+
+        assertEquals(expectedPopulation, actualPopulation);
+    }
+
+    @AfterClass
+    public static void close() {
+        app.disconnect();
     }
 }
